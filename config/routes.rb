da7656af3da1fb95b345f7558f042ea3d6ca2f94
Kamil Sopata <kamilsopata@gmail.com>
2017-12-16 19:08:58 +0100

    Add field to Devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
-  devise_for :users
-  
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
 end
