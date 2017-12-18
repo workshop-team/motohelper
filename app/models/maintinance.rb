c3687ea1a5d7ee96cc44de4244ae333727fe30a4
Fabian Kedzierski <grafska@gmail.com>
2017-12-18 14:31:56 +0100

    Fix rubocop issues

--- app/models/maintinance.rb
+++ app/models/maintinance.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Maintinance < ApplicationRecord
   belongs_to :car
   validates :name, presence: true
--- spec/factories/maintinances.rb
+++ spec/factories/maintinances.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :maintinance do
-    name "MyString"
+    name 'MyString'
     mileage 1
-    warranty_date "2017-12-17"
-    note "MyText"
+    warranty_date '2017-12-17'
+    note 'MyText'
     car nil
   end
 end
