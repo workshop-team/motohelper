f8c87159252a83eb1e8ddd90e98fdb6a39a084cd
Nikodem Gorak <nikos83@gmail.com>
2017-12-22 20:07:05 +0000

    Fix strange comments

--- app/dashboards/maintenance_dashboard.rb
+++ app/dashboards/maintenance_dashboard.rb
@@ -4,7 +4,6 @@ require 'administrate/base_dashboard'
 class MaintenanceDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
   # a hash that describes the type of each of the model's fields.
-  #
   # Each different type represents an Administrate::Field object,
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
@@ -21,7 +20,6 @@ class MaintenanceDashboard < Administrate::BaseDashboard
 
   # COLLECTION_ATTRIBUTES
   # an array of attributes that will be displayed on the model's index page.
-  #
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -4,8 +4,7 @@ require 'administrate/base_dashboard'
 
 class UserDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
+  # a hash that describes the type of each of the model's fields.s
   # Each different type represents an Administrate::Field object,
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
@@ -30,7 +29,6 @@ class UserDashboard < Administrate::BaseDashboard
 
   # COLLECTION_ATTRIBUTES
   # an array of attributes that will be displayed on the model's index page.
-  #
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
