16bf56bcffe7c646bf25211e6251ca8858ab27b8
Kamil Sopata <kamilsopata@gmail.com>
2017-12-23 22:42:25 +0100

    Add enum to model

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class Workshop < ApplicationRecord
+  enum kind: {
+    audi: 0, citroen: 1, fiat: 2, honda: 3, hyundai: 4, mazda: 5, opel: 6, peugeot: 7,
+    renault: 8, toyota: 9, volkswagen: 10, warsztat_lakierniczy: 11, warsztat_samochodowy: 12
+  }
 end
--- spec/models/workshop_spec.rb
+++ spec/models/workshop_spec.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Workshop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:kind, :latitude, :longitude) }
+  it { should have_many(:cars) }
 end
