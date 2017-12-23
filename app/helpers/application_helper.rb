c93f522c0cce4e37f4bf7686985c80fb3bf2ae3e
Nikodem Gorak <nikos83@gmail.com>
2017-12-23 07:24:03 +0000

    Add alert hleper, change flash message and add translation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    case flash_type
+    when 'success' then 'alert-success'
+    when 'error' then 'alert-danger'
+    when 'alert' then 'alert-warning'
+    when 'notice' then 'alert-info'
+    else
+      flash_type.to_s
+    end
+  end
 end
