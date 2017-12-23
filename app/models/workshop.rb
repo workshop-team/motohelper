faaab0075c047321e7cbf9b12311a3d827607ade
Kamil Sopata <kamilsopata@gmail.com>
2017-12-23 22:55:31 +0100

    Rspec fix

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Workshop < ApplicationRecord
+  validates :kind, :latitude, :longitude, presence: true
   enum kind: {
     audi: 0, citroen: 1, fiat: 2, honda: 3, hyundai: 4, mazda: 5, opel: 6, peugeot: 7,
     renault: 8, toyota: 9, volkswagen: 10, warsztat_lakierniczy: 11, warsztat_samochodowy: 12
--- spec/models/workshop_spec.rb
+++ spec/models/workshop_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Workshop, type: :model do
-  it { should validate_presence_of(:kind, :latitude, :longitude) }
-  it { should have_many(:cars) }
+  it { should validate_presence_of(:kind) }
+  it { should validate_presence_of(:latitude) }
+  it { should validate_presence_of(:longitude) }
 end
