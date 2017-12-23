253356495c6a48b44ae8f90183ae2798147be5ab
Kamil Sopata <kamilsopata@gmail.com>
2017-12-23 23:28:01 +0100

    Fixes

--- app/dashboards/workshop_dashboard.rb
+++ app/dashboards/workshop_dashboard.rb
@@ -1,4 +1,6 @@
-require "administrate/base_dashboard"
+# frozen_string_literal: true
+
+require 'administrate/base_dashboard'
 
 class WorkshopDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
@@ -9,11 +11,11 @@ class WorkshopDashboard < Administrate::BaseDashboard
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     id: Field::Number,
-    kind: Field::String.with_options(searchable: false),
+    kind: Field::String.with_options(searchable: true),
     latitude: Field::String.with_options(searchable: false),
     longitude: Field::String.with_options(searchable: false),
     created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    updated_at: Field::DateTime
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -21,30 +23,30 @@ class WorkshopDashboard < Administrate::BaseDashboard
   #
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
-  COLLECTION_ATTRIBUTES = [
-    :kind,
-    :latitude,
-    :longitude,
+  COLLECTION_ATTRIBUTES = %i[
+    kind
+    latitude
+    longitude
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
-  SHOW_PAGE_ATTRIBUTES = [
-    :id,
-    :kind,
-    :latitude,
-    :longitude,
-    :created_at,
-    :updated_at,
+  SHOW_PAGE_ATTRIBUTES = %i[
+    id
+    kind
+    latitude
+    longitude
+    created_at
+    updated_at
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
-  FORM_ATTRIBUTES = [
-    :kind,
-    :latitude,
-    :longitude,
+  FORM_ATTRIBUTES = %i[
+    kind
+    latitude
+    longitude
   ].freeze
 
   # Overwrite this method to customize how workshops are displayed
