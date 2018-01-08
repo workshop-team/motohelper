ee0b9196af55a7f859eb8241d7a1e9e1a43a8f92
Fabian Kedzierski <grafska@gmail.com>
2018-01-08 15:58:38 +0100

    Implement dynamic content to factories

--- spec/factories/reminders.rb
+++ spec/factories/reminders.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  init_mileage = 80_000
+  init_date = 6.months.ago
+
+  sequence(:reminder_mileage) { init_mileage += rand(200..400) }
+  sequence(:reminder_date) { init_date += rand(30..90).days }
+
   factory :reminder do
-    name 'Change motor'
-    mileage 145_000
-    reminder_date 12.months.since
-    note 'Note'
+    name { DataSets.reminder }
+    mileage { generate :reminder_mileage }
+    reminder_date { generate :reminder_date }
+    note { Faker::ChuckNorris.fact }
     car
   end
 end
