96391bbd333d86ff0e20ad5fe0b4a1ffd067eb5b
Kamil Sopata <kamilsopata@gmail.com>
2018-01-09 20:58:48 +0100

    Add rescue to Mileages show

--- app/controllers/mileages_controller.rb
+++ app/controllers/mileages_controller.rb
@@ -2,10 +2,13 @@
 
 class MileagesController < ApplicationController
   def index
-    @mileages = current_user.mileages.order(:car_id, meter_status: :asc).page params[:page]
+    @mileages = current_user.mileages.order(:car_id, created_at: :desc).page params[:page]
   end
 
   def show
     @mileage = current_user.mileages.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:error] = t 'errors.rescue.record_not_found'
+    redirect_to mileages_path
   end
 end
