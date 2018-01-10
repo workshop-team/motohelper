b38530befd01eb639771314a9625994678ec9e23
Kamil Sopata <kamilsopata@gmail.com>
2018-01-10 20:18:44 +0100

    Delete rescue

--- app/controllers/mileages_controller.rb
+++ app/controllers/mileages_controller.rb
@@ -2,13 +2,10 @@
 
 class MileagesController < ApplicationController
   def index
-    @mileages = current_user.mileages.order(:car_id, created_at: :desc).page params[:page]
+    @mileages = current_user.mileages.page params[:page]
   end
 
   def show
     @mileage = current_user.mileages.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    flash[:error] = t 'errors.rescue.record_not_found'
-    redirect_to mileages_path
   end
 end
