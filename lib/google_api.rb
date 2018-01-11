bc6f73f68a97d0ffc54a88cd198f66e0dc2cd7a6
Kamil Sopata <kamilsopata@gmail.com>
2018-01-11 21:08:10 +0100

    Change GoogleApi module

--- lib/google_api.rb
+++ lib/google_api.rb
@@ -1,30 +1,33 @@
+# frozen_string_literal: true
+
 require 'httparty'
 module GoogleApi
-  extend self
-
-  FIND_OBJECT_NEARBY_RADIUS = 50
+  FIND_OBJECT_NEARBY_RADIUS = 10
+  OBJECT_TYPES = 'car_repair'
+  MAPS_GOOGLEAPIS_URL = 'https://maps.googleapis.com/maps/api/place'
 
   def get_workshops_with_details(latitude, longitude)
     places = []
-    find_object_nearby(latitude, longitude)["results"].each do |object|
-      places << get_place_details(object['place_id'])["result"]
+    find_objects_nearby(latitude, longitude)['results'].each do |object|
+      places << get_place_details(object['place_id'])['result']
     end
     places
   end
 
-  def find_object_nearby(latitude, longitude)
-    response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?#{object_nearby_params(latitude, longitude)}")
-    body = JSON.parse(response.body)
+  def find_objects_nearby(latitude, longitude)
+    response = HTTParty.get("#{MAPS_GOOGLEAPIS_URL}/nearbysearch/json?#{object_nearby_params(latitude, longitude)}")
+    JSON.parse(response.body)
   end
 
   def get_place_details(place_id)
-    response = HTTParty.get("https://maps.googleapis.com/maps/api/place/details/json?#{place_details_params(place_id)}")
-    body = JSON.parse(response.body)
+    response = HTTParty.get("#{MAPS_GOOGLEAPIS_URL}/details/json?#{place_details_params(place_id)}")
+    JSON.parse(response.body)
   end
 
-private
+  private
+
   def object_nearby_params(latitude, longitude)
-    "location=#{latitude},#{longitude}&radius=#{FIND_OBJECT_NEARBY_RADIUS}&key=#{secrets}"
+    "location=#{latitude},#{longitude}&radius=#{FIND_OBJECT_NEARBY_RADIUS}&type=#{OBJECT_TYPES}&key=#{secrets}"
   end
 
   def place_details_params(place_id)
@@ -32,7 +35,9 @@ private
   end
 
   def secrets
-    ENV["GOOGLE_API_KEY"]
+    ENV['GOOGLE_API_KEY']
   end
 
+  module_function :get_workshops_with_details, :find_objects_nearby, :get_place_details,
+                  :object_nearby_params, :place_details_params, :secrets
 end
