9c6277f00bd8ee4bedd009ae08737d3872009631
Nikodem Gorak <nikos83@gmail.com>
2018-01-18 23:34:37 +0100

    Fix edit problems

--- app/assets/javascripts/datapicker.js
+++ app/assets/javascripts/datapicker.js
@@ -1,6 +1,7 @@
 $( document ).on('turbolinks:load', function() {
     $('.datepicker').datepicker({
         todayHighlight: true,
-        format: "dd-mm-yyyy"
+        format: "dd-mm-yyyy",
+        autoclose: true
     });
 })
