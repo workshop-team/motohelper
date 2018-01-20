901a2d1e557941955e8cc09470db8200b650a187
Fabian Kędzierski <grafska@gmail.com>
2018-01-20 18:30:13 +0100

    Remove test data

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-every 1.day, at: ['6:00 pm'] do
-  runner 'ReminderMailer.reminder_email(Reminder.last).deliver_later'
-end
-
 every 1.day, at: ['3:00 am'] do
   runner 'ReminderService.new.perform'
 end
