40cac0e63c38e2315cc52f4e4f363e42211e6b29
Nikodem Gorak <nikos83@gmail.com>
2018-01-21 23:33:35 +0100

    Add back button

--- app/uploaders/invoice_uploader.rb
+++ app/uploaders/invoice_uploader.rb
@@ -16,6 +16,9 @@ class InvoiceUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
+  def default_url
+    ActionController::Base.helpers.asset_path('invoices/' + [version_name, 'invoice.png'].compact.join('_'))
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
