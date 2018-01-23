73b56be34a70e05e8a916363081b7a8d680e1b1d
Fabian Kędzierski <grafska@gmail.com>
2018-01-23 15:20:50 +0100

    Add feature 'mileage' reminder

--- app/services/reminder_service.rb
+++ app/services/reminder_service.rb
@@ -3,18 +3,29 @@
 class ReminderService
   def initialize
     @reminder_days = 7
+    @reminder_mileage = 100
   end
 
   def perform
-    Reminder
-      .where(reminder_date: @reminder_days.day.since, sended: nil)
-      .each { |reminder| send_email(reminder) }
+    send_by_date
+    send_by_mileage
   end
 
-  def send_email(reminder)
-    ActiveRecord::Base.transaction do
-      reminder.update(sended: Date.today)
-      ReminderMailer.reminder_email(reminder).deliver_now
+  def send_by_date
+    Reminder
+      .where(reminder_date: @reminder_days.day.since, sended: nil)
+      .each { |reminder| send_email(reminder, :date) }
+  end
+
+  def send_by_mileage
+    Reminder.where(sended: nil).each do |reminder|
+      max_mileage = MaxMileage.value(reminder.car.id)
+      send_email(reminder, :mileage) if reminder.mileage < max_mileage + @reminder_mileage
     end
+  end
+
+  def send_email(reminder, type)
+    reminder.update(sended: Date.today)
+    ReminderMailer.reminder_email(reminder, type).deliver_now
   end
 end
