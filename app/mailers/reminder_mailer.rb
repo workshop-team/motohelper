ebe0eff13dfb332adf7c3472884fab73c176ac28
Fabian Kędzierski <grafska@gmail.com>
2018-01-23 15:21:35 +0100

    Modify mail views for Reminder

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class ReminderMailer < ApplicationMailer
-  def reminder_email(reminder)
+  def reminder_email(reminder, type)
     @user = reminder.car.user
     @reminder = reminder
+    @type = type
     mail(to: @user.email, subject: 'Motohelper - powiadomienie')
   end
 end
