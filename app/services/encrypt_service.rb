5434dab52370a8e0cb8f22f949fdbeb0f42251a2
Fabian Kędzierski <grafska@gmail.com>
2018-01-24 21:57:34 +0100

    Refactor code

--- app/services/encrypt_service.rb
+++ app/services/encrypt_service.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class EncryptService
-  @key = ActiveSupport::KeyGenerator.new('password').generate_key([ENV['MESSAGE_ENCRYPTOR_SALT']].pack('H*'), 32)
-  @encryptor = ActiveSupport::MessageEncryptor.new(@key)
+  key = ActiveSupport::KeyGenerator.new('password').generate_key([ENV['MESSAGE_ENCRYPTOR_SALT']].pack('H*'), 32)
+  @encryptor = ActiveSupport::MessageEncryptor.new(key)
 
   def self.encrypt_data(value)
     @encryptor.encrypt_and_sign(value)
