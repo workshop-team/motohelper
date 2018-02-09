7605231d2811232ccd4c8310926e5a2707c19c89
Fabian Kędzierski <grafska@gmail.com>
2018-02-09 11:07:26 +0100

    Refactor code

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -3,11 +3,6 @@
 require 'administrate/base_dashboard'
 
 class UserDashboard < Administrate::BaseDashboard
-  # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.s
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     cars: Field::HasMany,
     id: Field::Number,
@@ -34,10 +29,6 @@ class UserDashboard < Administrate::BaseDashboard
     )
   }.freeze
 
-  # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
     avatar
     name
@@ -45,8 +36,6 @@ class UserDashboard < Administrate::BaseDashboard
     language
   ].freeze
 
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
     avatar
     name
@@ -66,9 +55,6 @@ class UserDashboard < Administrate::BaseDashboard
     updated_at
   ].freeze
 
-  # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
     avatar
     name
